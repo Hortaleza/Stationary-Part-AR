555e51d1483a36585cfaad7366fafaacfe67acbe
KarlYu130 <klkyu@connect.ust.hk>
2023-11-15 23:52:01 +0800

    blutooth working

--- bluetooth.py
+++ bluetooth.py
@@ -3,13 +3,15 @@ import serial
 # MUST USE UART3 on the embedded system
 
 ser = serial.Serial(
-    port="COM9",  ## port name
+    port="COM7",  ## port name
     baudrate=115200,  ## baud rate
     bytesize=8,  ## number of databits
-    parity=serial.PARITY_EVEN,  ## enable parity checking
+    parity=serial.PARITY_NONE,  ## enable parity checking
     stopbits=1,  ## number of stopbits
     timeout=1,  ## set a timeout value, None for waiting forever
 )
-
-
+ser.write('V'.encode())
+#receivedData = ser.readline() 
+#print(receivedData)
 ser.close()  ## remember to close the port at last
+#0021:09:0011B0
