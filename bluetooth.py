2c91781cb48b847bff2a330172e20e419a0c4c41
KarlYu130 <klkyu@connect.ust.hk>
2023-11-17 21:34:43 +0800

    AR stationary finalized

--- bluetooth.py
+++ bluetooth.py
@@ -3,14 +3,14 @@ import serial
 # MUST USE UART3 on the embedded system
 
 ser = serial.Serial(
-    port="COM7",  ## port name
+    port="COM8",  ## port name
     baudrate=115200,  ## baud rate
     bytesize=8,  ## number of databits
     parity=serial.PARITY_NONE,  ## enable parity checking
     stopbits=1,  ## number of stopbits
     timeout=1,  ## set a timeout value, None for waiting forever
 )
-ser.write('V'.encode())
+ser.write('23'.encode())
 #receivedData = ser.readline() 
 #print(receivedData)
 ser.close()  ## remember to close the port at last
